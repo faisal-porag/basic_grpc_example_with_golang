34d0c1130a743278529b0e5976d59ffeb5017d69
faisal.porag <faisal.porag@technonext.com>
2023-08-24 09:56:38 +0600

    service updated

--- client/main.go
+++ client/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"google.golang.org/grpc/metadata"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"grpc_sample/details"
@@ -39,4 +42,68 @@ func main() {
 	fmt.Printf("Name: %s\n", response.Data.Name)
 	fmt.Printf("Age: %d\n", response.Data.Age)
 	fmt.Printf("Email: %s\n", response.Data.Email)
+}
+
+func GetDetailsInfoWithAuth() {
+	// Set up a connection to the server
+	conn, err := grpc.Dial("localhost:5005", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Attach the JWT token as a Bearer token to the gRPC request
+	token := generateServiceToken()
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization", "Bearer "+token)
+
+	// Create a new client
+	client := details.NewDetailsServiceClient(conn)
+
+	// Prepare the request
+	req := &details.DetailsRequest{
+		Name:  "John Doe",
+		Age:   30,
+		Email: "john@example.com",
+	}
+
+	// Call the GetDetails RPC
+	response, err := client.GetDetails(ctx, req)
+	if err != nil {
+		log.Fatalf("Error calling GetDetails: %v", err)
+	}
+
+	fmt.Printf("%v\n", response)
+
+	// Print the response
+	fmt.Printf("Name: %s\n", response.Data.Name)
+	fmt.Printf("Age: %d\n", response.Data.Age)
+	fmt.Printf("Email: %s\n", response.Data.Email)
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+// Create the JWT key used to create the signature
+var jwtKey = []byte("your_secret_key")
+
+func generateServiceToken() string {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return ""
+	}
+
+	return tokenString
 }
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"google.golang.org/grpc"
 	"grpc_sample/service_middleware"
 	"log"
 	"net"
 	"net/http"
+	"time"
 
 	"grpc_sample/details" // Adjust the import path
 )
@@ -88,6 +90,7 @@ func main() {
 
 	// Start HTTP server
 	http.HandleFunc("/api/rest/endpoint", handleRestRequest)
+	http.HandleFunc("/get-token", handleCreateTokenRequest)
 	fmt.Println("HTTP Server listening on port 8088")
 	if err := http.ListenAndServe(":8088", nil); err != nil {
 		fmt.Printf("Error: %v", err)
@@ -100,6 +103,51 @@ func handleRestRequest(w http.ResponseWriter, r *http.Request) {
 	// Simulate fetching data for the JSON response
 	data := map[string]interface{}{
 		"message": "Hello from REST API!",
+	}
+
+	// Convert the data map to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Send the JSON response
+	_, _ = w.Write(jsonData)
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+// Create the JWT key used to create the signature
+var jwtKey = []byte("your_secret_key")
+
+func handleCreateTokenRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Simulate fetching data for the JSON response
+	data := map[string]interface{}{
+		"token": tokenString,
 	}
 
 	// Convert the data map to JSON
